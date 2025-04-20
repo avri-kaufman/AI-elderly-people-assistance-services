06a0999d4e7735a16ffda62c060aec71578b9b75
avi <avrikaufman@gmail.com>
2025-04-20 13:06:48 +0300

    get help function with prompt to llm

--- server.js
+++ server.js
@@ -1,4 +1,25 @@
 const express = require("express");
+const llm = require("./llm_interaction");
 const app = express();
 
-app.listen(3000);
+PORT = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hi from server.js!"); // need to send an html file with the form for user
+});
+
+app.post("/get-assistance", (req, res) => {
+  payload = req.body;
+  if (payload === null || payload === undefined) {
+    res.status(400, "Bad request");
+  } else {
+    console.log("payload: ", payload);
+    llm.getHelp(payload, res);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server run on http://localhost:${PORT}`);
+});
